2d9bf1ceb9cc93528e4ecc707a9af8bbd5966e45
Pablo Costantini <pablo.costantini@southworks.com>
2017-01-06 11:24:07 -0300

    Update to botbuilder 3.5.1

--- Node/cards-CarouselCards/app.js
+++ Node/cards-CarouselCards/app.js
@@ -7,16 +7,15 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat bot
+// Create chat bot and list for messages
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Bot dialog
-bot.dialog('/', function (session) {
+// Bot setup
+var bot = new builder.UniversalBot(connector, function (session) {
     var cards = getCardsAttachments();
 
     // create reply with Carousel AttachmentLayout
