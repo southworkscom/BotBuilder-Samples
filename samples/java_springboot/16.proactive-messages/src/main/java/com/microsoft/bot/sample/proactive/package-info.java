045a61207d22f75c6c176dd51932202c504d7587
Victor <victor@grycuk.net>
2021-04-23 15:07:55 -0300

    Add package-info.java

---
+++ samples/java_springboot/16.proactive-messages/src/main/java/com/microsoft/bot/sample/proactive/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * This package contains the classes for the proactive messages sample.
+ */
+package com.microsoft.bot.sample.proactive;
